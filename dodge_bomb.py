c79faa7cbe09e436fb0ecc19862460dc3400fc2c
Soma Takayanagi <c0c2403966@edu.teu.ac.jp>
2025-11-25 14:41:24 +0900

    練習2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,15 +1,17 @@
 import os
+import random
 import sys
 import pygame as pg
 
 
 WIDTH, HEIGHT = 1100, 650
-DELTE = {
-    pg.k_up: (0,-5),
-    pg.K_DOWN: (0,+5),
-    pg.k_LEFT: (-5,0),
-    pg.k_RIGHT: (+5,0),
+DELTA = {
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0),
 }
+
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -20,6 +22,13 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+    bb_img = pg.Surface((20, 20))  
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  
+    bb_img.set_colorkey((0, 0, 0))  
+    bb_rct = bb_img.get_rect()  
+    bb_rct.centerx = random.randint(0, WIDTH)  
+    bb_rct.centery = random.randint(0, HEIGHT)  
+    vx, vy = +5, +5  
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -53,6 +62,8 @@ def main():
 
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx, vy)
+        screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
